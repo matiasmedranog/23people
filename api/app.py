c1dfdd27fe12b876b4f6c6531e5b6f2f5548bf91
Matías Medrano Gamonal <matiasmedrano94@gmail.com>
2020-08-02 13:02:25 +1000

    Basic flask app

---
+++ api/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv, find_dotenv
+from datetime import timedelta
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
+
+load_dotenv(find_dotenv())
+
+if __name__ == '__main__':
+    app.run(debug=os.getenv('DEBUG'),
+    host=os.getenv('HOST'), port=os.getenv('PORT'))
